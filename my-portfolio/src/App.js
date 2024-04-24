ab5c0c0720c1690360f0203de8f3066926e5d3c8
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-04-24 18:30:11 +0545

    About page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import { Link } from 'react-router-dom';
+import About from './components/About';
 
 function App() {
  return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          </Route>
+
       </Route>
     </Routes>
  );
---
+++ my-portfolio/src/components/About/index.js
@@ -0,0 +1,81 @@
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faAngular, faHtml5, faCss3, faReact, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+
+    useEffect(() => {
+        // Set a timeout to change the letterClass state after 4 seconds
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        // Return a cleanup function that clears the timeout
+        return () => clearTimeout(timer);
+    }, []); // Empty dependency array means this effect runs once on mount
+
+    return (
+
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                    />  </h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+                <p>
+                    orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                   
+                    <div className='face1'>
+                    <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                    </div>
+                    <div className='face2'>
+                    <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className='face3'>
+                    <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className='face4'>
+                    <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
+                    <div className='face5'>
+                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className='face6'>
+                    <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+
+
+
+    )
+
+}
+
+
+export default About
